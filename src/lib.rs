45306319f3cfbb5e357eb8252982479d5cf6e4cf
jordan8659 <andrew.m.jordan@outlook.com>
2020-06-23 01:10:36 -0500

    Added AVClient module and dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+use reqwest;
+use serde_json;
+
+const BASE_URL:&str = "https://www.alphavantage.co/query?";
+
+mod alpha_vantage_wrapper {
+
+    enum OutputSize {
+        Full, 
+        Compact,
+    }
+
+    struct AVClient<'a> {
+        interval: u8,
+        output_size: OutputSize,
+        api_key: &'a str,
+    }
+
+    impl<'b> AVClient<'b> {
+        pub fn new<'a>(interval: u8, output_size: OutputSize, api_key: &'a str) -> AVClient{
+            AVClient {
+                interval,
+                output_size,
+                api_key,
+            }
+        }
+    }
+
+    mod stock_time_series {
+        fn weekly_prices(fname: &str, stock_symbol: &str) -> String {
+            // dummy return
+            String::from("")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,3 +41,5 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+
