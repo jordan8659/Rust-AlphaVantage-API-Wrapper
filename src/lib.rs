941b6de0cdffadb087d43eb1723636ec4fe89df5
Drew <jordan.938@osu.edu>
2020-07-03 00:46:03 -0500

    Consolidated public wrapper module, incomplete GET functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,71 @@
 use reqwest;
 use serde_json;
 
-const BASE_URL:&str = "https://www.alphavantage.co/query?";
-
-mod alpha_vantage_wrapper {
-
-    enum OutputSize {
+pub mod wrapper {
+    pub enum OutputSize {
         Full, 
         Compact,
     }
 
-    struct AVClient<'a> {
-        interval: u8,
+    impl OutputSize {
+        fn value(&self) -> &str {
+            match *self {
+                OutputSize::Compact => "",
+                OutputSize::Full => "",
+            }
+        }
+    }
+
+    pub enum Interval {
+        OneMin,
+        FiveMin,
+        FiftheenMin,
+        ThirtyMin,
+        SixtyMin,        
+    }
+
+    impl Interval {
+        fn value(&self) -> &str {
+            match *self {
+                Interval::OneMin => "1min",
+                Interval::FiveMin => "5min",
+                Interval::FiftheenMin => "15min",
+                Interval::ThirtyMin => "30min",
+                Interval::SixtyMin => "60min",
+            }
+        }
+    }
+
+    pub struct IntradayRequest<'a> {
+        interval: Interval,
         output_size: OutputSize,
         api_key: &'a str,
     }
 
-    impl<'b> AVClient<'b> {
-        pub fn new<'a>(interval: u8, output_size: OutputSize, api_key: &'a str) -> AVClient{
-            AVClient {
+    impl<'b> IntradayRequest<'b> {
+        pub fn new<'a>(interval: Interval, output_size: OutputSize, api_key: &'a str) -> IntradayRequest{
+            IntradayRequest {
                 interval,
                 output_size,
                 api_key,
             }
         }
-    }
 
-    mod stock_time_series {
-        fn weekly_prices(fname: &str, stock_symbol: &str) -> String {
-            // dummy return
-            String::from("")
+        pub fn get_intraday_json(client: IntradayRequest, stock_symbol: &str) -> String {
+            let request_url = format!("https://www/alphavantage.co/query?function=TIME_SERIES_INTRADAY
+                &symbol={symbol}
+                &interval={interval}
+                &outputsize={output_size}
+                &apikey={api_key}",
+                symbol = stock_symbol,
+                interval = client.interval.value(),
+                output_size = client.output_size.value(),
+                api_key = client.api_key,
+            );
+            
+            //reqwest::blocking::get(&request_url).text() 
+
+            reqwest::blocking::get(&request_url)
         }
     }
 }
